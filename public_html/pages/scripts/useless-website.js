fec55c522cf8a36b818e6c78a906ae4bb2c4571a
alec <me@alec.ge>
2016-12-05 19:39:30 -0500

    added enter key

--- public_html/pages/scripts/useless-website.js
+++ public_html/pages/scripts/useless-website.js
@@ -2,3 +2,8 @@ function output() {
 	var x = document.getElementById("ayy");
 	document.getElementById("myOutput").innerHTML = x.value;
 }
+
+function triggerEnter() {
+	if (event.keyCode==13)
+		document.getElementById("submit").click();
+}
