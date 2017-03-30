c0add2dc3001b98f8024468306e893570d40c64c
Alec Ge <me@alec.ge>
2017-03-30 19:15:00 -0400

    primitive text flipper

--- public_html/pages/scripts/reverser.js
+++ public_html/pages/scripts/reverser.js
@@ -1,5 +1,4 @@
 function triggerEnter() {
-    "use strict";
 	if (event.keyCode === 13) {
 		document.getElementById("submit").click();
 	}
@@ -11,19 +10,53 @@ function reverse() {
     console.log(input);
     var temp = "";
 
-    for (var i = input.length - 1; i >= -1; i -= 1) {
+    for (var i = input.length - 1; i > -1; i -= 1) {
         temp += input.charAt(i);
     }
-    
+
     if (document.getElementById("replaceChar").checked) {
-        
+      temp = replaceChar(temp);
     }
-    
+
     document.getElementById("output").innerHTML = temp;
 }
 
-function replaceChar() {
-    var mapping = { "e": "ɘ", } 
-    
-    
+function replaceChar(original) {
+    var mapping = { "a": "ɐ",
+                    "b": "q",
+                    "c": "ɔ",
+                    "d": "p",
+                    "e": "ǝ",
+                    "f": "ɟ",
+                    "g": "ƃ",
+                    "h": "ɥ",
+                    "i": "ᴉ",
+                    "j": "ɾ",
+                    "k": "ʞ",
+                    "l": "l",
+                    "m": "ɯ",
+                    "n": "u",
+                    "o": "o",
+                    "p": "d",
+                    "q": "b",
+                    "r": "ɹ",
+                    "s": "s",
+                    "t": "ʇ",
+                    "u": "n",
+                    "v": "ʌ",
+                    "w": "ʍ",
+                    "x": "x",
+                    "y": "ʎ",
+                    "z": "z" };
+
+    var jsonified = JSON.stringify(mapping);
+    localStorage.setItem("map", jsonified);
+    stringified = localStorage.getItem("map");
+
+    var reverseTemp = "";
+    for (var i = 0; i < original.length; i += 1) {
+      reverseTemp += mapping[original.charAt(i)];
+    }
+
+    return reverseTemp;
 }
